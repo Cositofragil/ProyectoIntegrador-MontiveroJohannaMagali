936f2a97a123fad88134aaffd485fd8d9d74bffc
Cositofragil <magy_1398@hotmail.com>
2022-10-29 19:05:31 -0300

    make it better

---
+++ backend/src/main/java/com/portfolio/mjm/MjmApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mjm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjmApplication.class, args);
+	}
+
+}
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'mjmfrontend-13bf2',
+    appId: '1:82213033108:web:fcc85ab3ede8154685d081',
+    storageBucket: 'mjmfrontend-13bf2.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyBXNcQK1BP_nTcff17BpZaNRaVMW66ftJI',
+    authDomain: 'mjmfrontend-13bf2.firebaseapp.com',
+    messagingSenderId: '82213033108',
+    measurementId: 'G-0K391RFZKE',
+  },
+  production: false,
+  URL:'http://localhost:8080/'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
