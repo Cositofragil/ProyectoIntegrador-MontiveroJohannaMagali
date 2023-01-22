2f6fef2532d7ac7f6dd72117746368cb8ed497d0
Cositofragil <100120872+Cositofragil@users.noreply.github.com>
2023-01-22 19:57:08 -0300

    Update CEducacion.java

--- ProyectoIntegrador-MontiveroJohannaMagali/backend/src/main/java/com/portfolio/mjm/Controller/CEducacion.java
+++ ProyectoIntegrador-MontiveroJohannaMagali/backend/src/main/java/com/portfolio/mjm/Controller/CEducacion.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.portfolio.mjm.Controller;
 
 import com.portfolio.mjm.Dto.dtoEducacion;
@@ -25,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins ="http://localhost:4200")
 public class CEducacion {
     @Autowired
     SEducacion sEducacion;
@@ -38,7 +35,7 @@ public class CEducacion {
     @GetMapping("/detail/{id}")
     public ResponseEntity<Educacion> getById(@PathVariable("id")int id){
         if(!sEducacion.existsById(id)){
-            return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity(new Mensaje("No existe el ID"), HttpStatus.NOT_FOUND);
         }
         
         Educacion educacion = sEducacion.getOne(id).get();
